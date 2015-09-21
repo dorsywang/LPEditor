f8be7950a9e906e53a710f7492cf9f7cb9417a61
dorsywang <honghu91@hotmail.com>
2015-09-21 20:07:59 +0800

    add

--- src/litepage_editor.js
+++ src/litepage_editor.js
@@ -660,7 +660,7 @@
 
             var cssLink = this.cssLink;
 
-            this.head.innerHTML += "<style type='text/css'>html,body{width: 100%;height: 100%;}</style>";
+            this.head.innerHTML += "<style type='text/css'>html,body{width: 100%;height: 100%;overflow:hidden;}</style>";
 
             if(cssLink){
                 this.head.innerHTML += '<link media="all" rel="stylesheet" href="' + cssLink + '" type="text/css" />';
